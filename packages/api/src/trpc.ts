8404c95046e52b2adb65eab0e5644a9025101788
Joël Kuijper <jmkuijper@outlook.com>
2024-02-12 13:20:21 +0100

    feat: init crawlers

--- packages/api/src/trpc.ts
+++ packages/api/src/trpc.ts
@@ -6,8 +6,7 @@
  * tl;dr - this is where all the tRPC server stuff is created and plugged in.
  * The pieces you will need to use are documented accordingly near the end
  */
-import { TRPCError, initTRPC } from '@trpc/server';
-import { bearerAuth } from 'hono/bearer-auth';
+import { initTRPC } from '@trpc/server';
 import superjson from 'superjson';
 import { ZodError } from 'zod';
 
@@ -23,8 +22,8 @@ import { ZodError } from 'zod';
  *
  * @see https://trpc.io/docs/server/context
  */
-export const createTRPCContext = async (opts: { headers: Headers; session: Session }) => {
-  const session = opts.session ?? bearerAuth({ token: 'hello world' });
+export const createTRPCContext = async (opts: { headers: Headers }) => {
+  // const session = opts.session ?? (await auth());
   const source = opts.headers.get('x-trpc-source') ?? 'unknown';
 
   console.log('>>> tRPC Request from', source);
@@ -88,13 +87,13 @@ export const publicProcedure = t.procedure;
  * @see https://trpc.io/docs/procedures
  */
 export const protectedProcedure = t.procedure.use(({ ctx, next }) => {
-  if (!ctx.session?.user) {
-    throw new TRPCError({ code: 'UNAUTHORIZED' });
-  }
+  // if (!ctx.session?.user) {
+  //   throw new TRPCError({ code: 'UNAUTHORIZED' });
+  // }
   return next({
     ctx: {
       // infers the `session` as non-nullable
-      session: { ...ctx.session, user: ctx.session.user },
+      // session: { ...ctx.session, user: ctx.session.user },
     },
   });
 });
